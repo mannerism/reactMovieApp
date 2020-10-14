4e9f5dbb5223ba3df793fa78ecf17a47faedea86
Juno Yu <jxy35@hitit.xyz>
2020-10-14 22:36:02 +0900

    completed fetching data, genres b4 applying css

--- src/movie.js
+++ src/movie.js
@@ -1,13 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+          <li key={index} className="genres__genre">{genre}</li>
+          ))}
+        </ul>
         <p className="movie__paragraph">{summary}</p>
       </div>
     </div>
@@ -20,6 +26,7 @@ Movie.propTypes = {
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
   year: PropTypes.number.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
