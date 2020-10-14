aed958566ef2e33ebb6eb119ed7fc57f22843084
Juno Yu <jxy35@hitit.xyz>
2020-10-14 21:27:24 +0900

    finished with practice, going to create an actual project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import React from "react";
 
 class App extends React.Component {
-  //React automatically is going to execute the render function of your class component
-  //The reason why we are using a class component is because of 'state'
-  state = {
-    count: 0,
-  };
-  add = ()  => {
-    // this.state.count = 1; this is not recommended to set state because this won't call render function. You should use setstate instead.
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = ()  => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
   render() {
-    return (
-    <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
-    )
+    return <div />;
   }
 }
 
